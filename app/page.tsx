1ffbd12933dcf4b05731e7928d2b4e72d7793558
Aryan Bola <aaryann5002@gmail.com>
2025-07-12 15:36:11 +0530

    a major part done, made the div add on click, maybe will make then dragable by motin but need to study that, url and canvas size working, clip parth working, just need some setting

--- app/page.tsx
+++ app/page.tsx
@@ -1,103 +1,110 @@
-import Image from "next/image";
+"use client"
+import { useState } from "react";
+import shapes from './shapes.json'
+import pictures from './pictures.json'
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+  const [copyButton, setCopyButton] = useState(false)
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+  interface DivData {
+    id: number,
+    xPosition:number,
+    yPosition:number
+  };
+
+  const [divs, setDivs] = useState<DivData[]>([]);
+  const [canvasSize, setCanvasSize] = useState({x:480,y:480})
+  const [bgImage,setBgImage] = useState('/Zoro.jpeg')
+
+  function addDiv(e: React.MouseEvent<HTMLDivElement>) {
+
+    const container = e.currentTarget.getBoundingClientRect();
+    const x = e.clientX - container.left;
+    const y = e.clientY - container.top;
+
+    setDivs([...divs, { id: Date.now(),xPosition:x, yPosition:y }]);
+    console.log(divs,'divs')
+    console.log(container,"container")
+  }
+
+  return (
+    <>
+    <div className="h-screen w-full bg-black flex-col-reverse flex gap-2 md:flex-row justify-between items-center">
+      <div className="w-[30%] h-full border border-white rounded-2xl">
+        {/* shapes */}
+        <div className="p-4 grid grid-cols-4 gap-4 place-items-center">
+          {
+            shapes.map((i,index)=>(
+              <button key={index} className="w-16 h-16 border border-white text-white text-xs flex flex-col justify-center items-center"><img className="w-12 h-12" src={i.img_name} alt='' />{i.name}</button>
+            ))
+          }
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+        {/* demo sixe */}
+        <div className="px-4">
+          <p className="text-white text-xl py-2">Demo size <span className="text-gray-500">(in px)</span></p>
+          <div className="flex justify-around items-center gap-4">
+            <input value={canvasSize.x} onChange={(e) => setCanvasSize(prev => ({...prev, x: parseInt(e.target.value)}))} type="text" className="rounded-2xl border border-white py-2 text-white px-4 overflow-hidden w-20" placeholder="320px"/>
+            <p className="text-white text-xl">X</p>
+            <input value={canvasSize.y} onChange={(e) => setCanvasSize(prev => ({...prev, y: parseInt(e.target.value)}))} type="text" className="rounded-2xl border border-white py-2 text-white px-4 overflow-hidden w-20" placeholder="320px"/>
+          </div>
+        </div>
+        {/* custum image */}
+        <div className="px-4 grid gap-4">
+          <div>
+            <p className="text-white text-xl py-2">Custum Image</p>
+            <div className="flex justify-between items-center ">
+              {
+                pictures.map((pic,index)=>(
+                  <button key={index} onClick={()=>setBgImage(pic.img_name)} className="w-16 h-16 border-white border"><img src={pic.img_name} alt="" /></button>
+                ))
+              }
+            </div>
+          </div>
+          <p></p>
+          <input value={bgImage} onChange={(e)=>setBgImage(e.target.value)} type="text" className="border border-white py-2 rounded-2xl w-full text-white px-4 overflow-hidden" placeholder="Custom URL" />
+        </div>
+        <div className="rounded-2xl px-4">
+          <p className="text-white text-xl py-2 px-4 text-center">Made by Aryan Bola</p>
+        </div>
+      </div>
+      <div className="h-full w-full grid gap-2">
+        <div className="row-span-12 h-full w-full grid gap-2">
+          <div className="px-4 py-2 flex justify-between items-center rounded-2xl border border-white row-span-1">
+            <p className="text-white text-2xl font-semibold /80">PathClipper</p>
+            <p className="text-white text-xl ">By Aryan Bola</p>
+          </div>
+          <div className="h-full border border-white rounded-2xl w-full flex justify-center items-center row-span-10 overflow-hidden">
+            <div 
+              className="w-80 h-80 border border-white relative" 
+              style={{
+                width:canvasSize.x,
+                height:canvasSize.y,
+                backgroundImage:`url(${bgImage})`,
+                backgroundSize: 'cover',
+                backgroundPosition: 'center'
+              }} 
+              onClick={(e)=>addDiv(e)}
+            >
+              {divs.map((div, index) => (
+                <div key={div.id} className="w-4 h-4 rounded-full bg-red-500 absolute" style={{top:div.yPosition,left:div.xPosition}} />
+              ))}
+            </div>
+          </div>
+        </div>
+        <div className="h-full w-full row-span-2 flex justify-start items-center px-4 border border-white bg-black rounded-2xl relative overflow-hidden">
+          <p className="text-white text-lg font-medium flex gap-2 flex-wrap">
+            clip-path: path(
+            {
+              divs.map(div=>(
+                <span key={div.id}>{div.xPosition},{div.yPosition}</span>
+              ))
+            }
+            );
+          </p>
+          <button className="absolute top-2 right-2 border border-white px-4 py-2 rounded-2xl bg-black text-white tex-xl cursor-pointer" onClick={()=>setCopyButton(true)}>{copyButton ? 'Copied' : 'Copy'}</button>
+        </div>
+      </div>
     </div>
+    </>
   );
 }
